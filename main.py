e598b2f7fc573a555a5eada6d6649b1bfa4a31c6
Yulia Sheynkman <Yulias@Kayhut.local>
2020-11-02 00:31:07 -0800

    CR_2 to part 2

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import psutil
 import win32api
+import win32con
 import win32process
 import win32gui
 import argparse
 
 
+# pid = None
+
 def build_parser():
     parser = argparse.ArgumentParser(description='This is a title changer')
-    parser.add_argument('win_name', action="store", type=str)
+    parser.add_argument('proc_name', action="store", type=str)
     parser.add_argument('new_title', action="store", type=str)
 
     parse_results = parser.parse_args()
@@ -14,13 +18,20 @@ def build_parser():
     return parse_results
 
 
-def find_it(hwnd, parser):
-    if parser.win_name in win32gui.GetWindowText(hwnd):
-        win32gui.SetWindowText(hwnd, parser.new_title)
+def enum_handler(hwnd, ll):
+    _, p = win32process.GetWindowThreadProcessId(hwnd)
+    if p is not None and p == ll[0]:
+        win32gui.SetWindowText(hwnd, ll[1])
+
 
 def main():
     parser = build_parser()
-    win32gui.EnumWindows(find_it, parser)
+
+    for proc in psutil.process_iter():
+        if parser.proc_name in proc.name():
+            pid = proc.pid
+
+    win32gui.EnumWindows(enum_handler, [pid, parser.new_title])
 
 
 if __name__ == "__main__":
